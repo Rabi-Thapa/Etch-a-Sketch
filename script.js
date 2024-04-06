fe0d0405db83d2594747d6d607cf9666b98a6659
RABI <thaparabi8848@gmail.com>
2024-04-06 22:58:05 +0545

    on progress

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
 const divContainer = document.getElementById('main-container')
 const btnAdd = document.querySelector(".btn-add")
+const btnBlue = document.querySelector(".btn-blue")
 btnAdd.addEventListener("click", makeSquare);
 
+btnBlue.addEventListener('click', chooseColor)
+
+
+function chooseColor(){
+    let boxes = [...document.querySelectorAll('.square')]
+
+    boxes.forEach(box => {
+        box.addEventListener('mouseover', () =>{
+            box.style.backgroundColor = 'blue'
+        });
+    });
+}
 
 var slider = document.getElementById("myRange")
 var output = document.getElementById("demo")
-output.innerHTML = slider.value; // Display
+output.innerHTML = slider.value;
 
 slider.oninput = function () {
     output.innerHTML = this.value;
     square = this.value;
 }
 
-let square=8;
+let square=16;
 function makeSquare() {
     divContainer.innerHTML = "";
     for (let i = 0; i < square*square; i++) {
@@ -26,32 +39,53 @@ function makeSquare() {
 
 function addEventListen() {
     let boxes = [...document.querySelectorAll('.square')]
-    let draw = false;
+    // let draw = false;
+
     boxes.forEach(box => {
-        // box.addEventListener('mousedown', () => {
-        //     box.style.backgroundColor = 'drakgray'
-        //     draw = true;
-        // })
-
-        
-        // box.addEventListener('mouseup', () => {
-        //     draw = false;
-        // });
-
         box.addEventListener('mouseover', () =>{
-            box.style.backgroundColor = 'darkgray'
+            box.style.backgroundColor = 'black'
             // draw = true;
         });
-
-        // box.addEventListener('mouseout', () =>{
-        //     if(draw){
-        //         box.style.backgroundColor = 'blue'
-        //         // draw = true;
-        //     }
-        // });
     });
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ // box.addEventListener('mousedown', () => {
+    //     box.style.backgroundColor = 'drakgray'
+    //     draw = true;
+    // })
+
+    
+    // box.addEventListener('mouseup', () => {
+    //     draw = false;
+// });
+
+
+ // box.addEventListener('mouseout', () =>{
+    //     if(draw){
+    //         box.style.backgroundColor = 'blue'
+    //         // draw = true;
+    //     }
+// });
 
 
 
